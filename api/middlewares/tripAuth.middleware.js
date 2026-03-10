f52fa310ce4cfef0236a515abe6aac15ee6fa8d4
rafacruiz <rafacruiz@gmail.com>
2026-03-10 15:52:29 +0100

    feat: update checkTripAuth to support traveler permissions per route

--- api/middlewares/tripAuth.middleware.js
+++ api/middlewares/tripAuth.middleware.js
@@ -9,18 +9,35 @@ export async function checkTripAuth(req, res, next) {
 
     if (!trip) throw createHttpError(404, "Trip not found");
 
+    const isTraveler = trip.travelers.some(
+        t => t.user.toString() === req.session.user.id
+    );
+
     const userId = req.session.user.id;
+
     const isOwner = trip.userOwner.toString() === userId.toString();
 
-    if (!isOwner && ['POST', 'PATCH', 'DELETE'].includes(req.method)) {
-        throw createHttpError(403, "You don't have access to this trip");
+    if (!isOwner && !isTraveler) {
+        return next(createHttpError(403, "You don't have access to this trip"));
     }
 
-    if (!isOwner && req.method === 'PATCH' && trip.endDate.getTime() < Date.now()) {
-        throw createHttpError(403, "You can't modify a trip that has already ended");
+    if (!isOwner && isTraveler) {
+
+        if (trip.endDate.getTime() < Date.now()) {
+            throw createHttpError(400, "Cannot add places to a finished trip");
+        }
+
+        const allowedForTraveler = ['places', 'activities']; // Añadir a env
+
+        const resource = req.baseUrl.split("/").pop();
+
+        if (!allowedForTraveler.includes(resource)) {
+            return next(createHttpError(403, "Traveler not allowed for this resource"));
+        }
     }
 
     req.trip = trip;
+    req.tripRole = isOwner ? "owner" : "traveler";
 
     next();
 };
