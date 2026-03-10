c0a46b12657d1853c47cdefe3b3e8670ed8643a9
rafacruiz <rafacruiz@gmail.com>
2026-03-10 14:51:58 +0100

    feat: new controller Places

--- api/controllers/places.controller.js
+++ api/controllers/places.controller.js
@@ -1,41 +1,51 @@
 
 import createHttpError from "http-errors";
 import Place from '../models/place.model.js';
-import Trip from '../models/trip.model.js';
 
 export async function create(req, res) {
 
-    const trip = await Trip.findById(req.body.trip);
-    
-    if (!trip) throw createHttpError(404, "Trip not found");
+    const { name, location, notes } = req.body;
 
-    if (trip.endDate < new Date()) 
+    const tripCurrent = req.trip;
+
+    if (tripCurrent.endDate.getTime() < Date.now()) {
         throw createHttpError(400, "Cannot add places to a finished trip");
+    }
 
-    const isTraveler = trip.travelers.some(
+    const isTraveler = tripCurrent.travelers.some(
         t => t.user.toString() === req.session.user.id
     );
 
-    if (!isTraveler && trip.userOwner.toString() !== req.session.user.id) {
+    if (!isTraveler && tripCurrent.userOwner.toString() !== req.session.user.id) {
         throw createHttpError(403, "You are not part of this trip");
     }
-
-    const placeData = { 
-        "name": req.body.name, 
-        "location": req.body.location, 
-        "notes": req.body.notes, 
-        "trip":  req.body.trip
-    }
     
-    const place = await Place.create(placeData);
+    const place = await Place.create({ name, location, notes, trip: tripCurrent.id });
     
-    res.json(place);
+    res.json({ success: true, data: {
+        name: place.name,
+        location: place.location,
+        notes: place.notes,
+        trip: place.trip
+    } });
 }
 
 export async function update(req, res) {
-    
+
+    const place = await Place.findById(req.params.placeId);
+
+    if (!place) throw createHttpError(404, "Place not found");
+
+    Object.assign(place, { visited: !place.visited });
+
+    await place.save();
+
+    res.json({ success: true });
 }
 
 export async function remove(req, res) {
     
+    await Place.findByIdAndDelete(req.params.placeId);
+
+    res.status(204).end();
 }
