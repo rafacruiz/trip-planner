e99162293cbd86501f3ba2055ea17eca56972278
Rafacruiz <rafacruiz@gmail.com>
2026-03-10 21:56:38 +0100

    chore(route): new name fuction checkTripMemberAuth

--- api/config/routes.config.js
+++ api/config/routes.config.js
@@ -2,7 +2,8 @@
 import { Router } from "express";
 import createHttpError from "http-errors";
 
-import { checkTripAuth } from '../middlewares/tripAuth.middleware.js';
+import { checkTripAuth } from '../middlewares/ownerAuth.middleware.js';
+import { checkTripMemberAuth } from '../middlewares/tripMemberAuth.middleware.js';
 
 import * as User from '../controllers/users.controller.js';
 import * as Trip from '../controllers/trips.controller.js'; 
@@ -27,13 +28,13 @@ router.delete('/trips/:tripId', checkTripAuth, Trip.remove);
 router.patch('/trips/:tripId/add-traveler', checkTripAuth, Trip.addTraveler);
 router.patch('/trips/:tripId/remove-traveler', checkTripAuth, Trip.delTraveler);
 
-router.post('/trips/:tripId/places', checkTripAuth, Place.create);
-router.patch('/trips/:tripId/places/:placeId', checkTripAuth, Place.update);
-router.delete('/trips/:tripId/places/:placeId', checkTripAuth, Place.remove);
+router.post('/trips/:tripId/places', checkTripMemberAuth, Place.create);
+router.patch('/trips/:tripId/places/:placeId', checkTripMemberAuth, Place.update);
+router.delete('/trips/:tripId/places/:placeId', checkTripMemberAuth, Place.remove);
 
-router.post('/trips/:tripId/activities', checkTripAuth, Activity.create);
-router.patch('/trips/:tripId/activities/:activityId', checkTripAuth, Activity.update);
-router.delete('/trips/:tripId/activities/:activityId', checkTripAuth, Activity.remove);
+router.post('/trips/:tripId/activities', checkTripMemberAuth, Activity.create);
+router.patch('/trips/:tripId/activities/:activityId', checkTripMemberAuth, Activity.update);
+router.delete('/trips/:tripId/activities/:activityId', checkTripMemberAuth, Activity.remove);
 
 router.use((req, res) => {
   throw new createHttpError(404, "Route Not Found");
