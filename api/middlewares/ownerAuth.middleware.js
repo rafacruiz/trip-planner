364240b7674aa7a275c31d807c4f12a69b8dbec6
Rafacruiz <rafacruiz@gmail.com>
2026-03-10 21:54:27 +0100

    feat: middleware Roles Auth

---
+++ api/middlewares/ownerAuth.middleware.js
@@ -0,0 +1,24 @@
+
+
+import createHttpError from "http-errors";
+import Trip from '../models/trip.model.js';
+
+export async function checkTripAuth(req, res, next) {
+
+    const trip = await Trip.findById(req.params.tripId);
+
+    if (!trip) throw createHttpError(404, "Trip not found");
+
+    const userId = req.session.user.id;
+
+    const isOwner = trip.userOwner.toString() === userId.toString();
+
+    if (!isOwner) {
+        return next(createHttpError(403, "You don't have access to this trip"));
+    }
+
+    req.trip = trip;
+    req.tripRole = isOwner;
+
+    next();
+};
---
+++ api/middlewares/tripMemberAuth.middleware.js
@@ -0,0 +1,31 @@
+
+import createHttpError from "http-errors";
+import Trip from '../models/trip.model.js';
+
+export async function checkOtherAuth(req, res, next) {
+
+    const trip = await Trip.findById(req.params.tripId);
+
+    if (!trip) throw createHttpError(404, "Trip not found");
+
+    if (trip.endDate.getTime() < Date.now()) {
+        throw createHttpError(400, "Cannot add places to a finished trip");
+    }
+
+    const isTraveler = trip.travelers.some(
+        t => t.user.toString() === req.session.user.id
+    );
+
+    const userId = req.session.user.id;
+
+    const isOwner = trip.userOwner.toString() === userId.toString();
+
+    if (!isOwner && !isTraveler) {
+        return next(createHttpError(403, "You don't have access to this trip"));
+    }
+
+    req.trip = trip;
+    req.tripRole = isOwner ? 'owner' : 'traveler';
+
+    next();
+};
