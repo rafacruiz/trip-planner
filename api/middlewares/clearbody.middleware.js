ec9d210d8072f497254fcaec7d7b987b12d9775c
Rafacruiz <rafacruiz@gmail.com>
2026-03-12 20:18:12 +0100

    feat: check body empty request post, patch and put

--- api/middlewares/clearbody.middleware.js
+++ api/middlewares/clearbody.middleware.js
@@ -1,5 +1,11 @@
 
+import createHttpError from "http-errors";
+
 export function clearBody(req, res, next) {
+
+    if (!req.body && req.method.includes(['POST', 'PATCH', 'PUT'])) {
+        throw createHttpError(400, 'No fields provided for update');
+    }
     
     delete req.body?._id;
     delete req.body?.createdAt;
