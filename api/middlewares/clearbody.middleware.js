220bbb98fc6528a1accae13567ea220562f92481
Rafacruiz <rafacruiz@gmail.com>
2026-03-18 23:17:44 +0100

    refactor(middleware): handle multipart/form-data headers in clearBody

--- api/middlewares/clearbody.middleware.js
+++ api/middlewares/clearbody.middleware.js
@@ -3,7 +3,13 @@ import createHttpError from "http-errors";
 
 export function clearBody(req, res, next) {
 
-    if (!req.body && ['POST', 'PATCH', 'PUT'].includes(req.method)) {
+    if (req.headers['content-type']?.includes('multipart/form-data')) {
+        return next();
+    }
+
+    const hasBody = req.body && Object.keys(req.body).length > 0;
+
+    if (!hasBody && ['POST', 'PATCH', 'PUT'].includes(req.method)) {
         throw createHttpError(400, '¡No fields provided, body empty!');
     }
     
