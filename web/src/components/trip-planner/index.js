94542fcdef324e595c0dd566bf9a3b961c9cacd1
Rafacruiz <rafacruiz@gmail.com>
2026-03-20 00:56:55 +0100

    feat: new trips components

--- web/src/components/trip-planner/index.js
+++ web/src/components/trip-planner/index.js
@@ -4,3 +4,4 @@ export { default as TripsItem } from './trips/trips-item/trips-item';
 export { default as TripsDetails } from './trips/trips-details/trips-details';
 export { default as TripDate } from './trips/trips-utils/date-trips';
 export { default as TripsSlider } from './trips/trips-slider/trips-slider';
+export { default as TripsSearch } from './trips/trips-search/trips-search';
