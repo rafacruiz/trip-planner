2cdf1acfa63eb9bf7b83da303262653bf9ba2a47
Rafacruiz <rafacruiz@gmail.com>
2026-03-19 18:02:55 +0100

    feat: add user page

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -3,5 +3,6 @@ export { default as LoginPage } from './login-page';
 export { default as SignupPage } from './signup-page';
 export { default as DashboardPage } from './dashboard-page';
 export { default as ProfilePage } from './profile-page';
-export { default as ProfileEditPage } from './profile-edit-page';
+export { default as UserPage } from './user-page';
 export { default as TripsPage } from './trips-page';
+export { default as NotFoundPage } from './404-page';
---
+++ web/src/pages/user-page.jsx
@@ -0,0 +1,13 @@
+
+import { LayoutPage } from "../components/layouts";
+import { UserDetails } from "../components/users";
+
+function UserPage() {
+    return (
+        <LayoutPage>
+            <UserDetails/>
+        </LayoutPage>
+    );
+}
+
+export default UserPage;
