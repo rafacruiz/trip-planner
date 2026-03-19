c297201485dfd48ab8156b157738d44981dfb663
Rafacruiz <rafacruiz@gmail.com>
2026-03-20 00:59:47 +0100

    feat: new pages

---
+++ web/src/pages/ExploreTripsPage.jsx
@@ -0,0 +1,14 @@
+
+import { LayoutPage } from "../components/layouts";
+import { TripsSearch } from "../components/trip-planner";
+
+function ExploreTripsPage() {
+
+   return (
+        <LayoutPage>
+            <TripsSearch />
+        </LayoutPage>
+    );
+}
+
+export default ExploreTripsPage;
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -5,4 +5,5 @@ export { default as DashboardPage } from './dashboard-page';
 export { default as ProfilePage } from './profile-page';
 export { default as UserPage } from './user-page';
 export { default as TripsPage } from './trips-page';
+export { default as ExploreTripsPage } from './ExploreTripsPage';
 export { default as NotFoundPage } from './404-page';
