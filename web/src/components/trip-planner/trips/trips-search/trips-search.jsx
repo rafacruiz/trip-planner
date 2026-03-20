05b72ff15c2eb971f09925303f91d8038b914154
Rafacruiz <rafacruiz@gmail.com>
2026-03-20 22:48:48 +0100

    feat: search, filters and pagination components

--- web/src/components/trip-planner/trips/trips-search/trips-search.jsx
+++ web/src/components/trip-planner/trips/trips-search/trips-search.jsx
@@ -1,29 +1,62 @@
 
-import { useEffect, useRef, useState } from 'react';
-import { ButtonSearch } from '../../../ui';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { ButtonSearch, Loading, Pagination } from '../../../ui';
 import TripsList  from "../trips-list/trips-list";
 import useTrips from "../../../../hooks/use-trips";
 
 function TripsSearch() {
 
+    const location = useLocation();
+
+    const params = new URLSearchParams(location.search);
+
     const [filters, setFilters] = useState({
-        search: ''
-    })
-    
-    const searchParams = new URLSearchParams(location.search);
-    
-    const {trips, loading, error } = useTrips(filters);
+        search: '',
+        trips: params.get('trips') || '',
+        me: params.get('me') || '',
+        owner: params.get('owner') || '',
+        page: 1,
+        limit: 10,
+    });
+
+    if (!filters.search) delete filters.search;
+    if (!filters.trips) delete filters.trips;
+    if (!filters.me) delete filters.me;
+    if (!filters.owner) delete filters.owner;
+  
+    const { trips, loading, error, pagination } = useTrips(filters);
+
+    if (pagination?.totalPages === 1) filters.page = 1;
+
+    const handlePageChange = (newPage) => {
+        setFilters({ ...filters, page: newPage});
+    };
    
     return (
         <>
             <div className="pb-10">
-                <ButtonSearch filters={ filters } setFilters={ setFilters } />
+                <ButtonSearch 
+                    filters={ filters } 
+                    setFilters={ setFilters } 
+                />
             </div>
 
-            { trips && 
-                <TripsList trips={ trips } loading={ loading } />
-            }
-            
+            { loading && <Loading /> }
+
+            { trips && (
+                
+                    <TripsList trips={ trips } loading={ loading } />
+            )}
+
+            { trips?.length !== 0 && (
+                
+                    <Pagination
+                        pagination={ pagination }
+                        onPageChange={ handlePageChange }
+                    />              
+                
+            )}
         </>
     );
 }
