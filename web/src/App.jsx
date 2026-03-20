d79b35070a39c3e984ef8bc6f45472a6c9a681ef
Rafacruiz <rafacruiz@gmail.com>
2026-03-20 01:00:44 +0100

    feat: change app.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { Routes, Route } from 'react-router';
 
-import { DashboardPage, LoginPage, NotFoundPage, ProfilePage, SignupPage, TripsPage, UserPage } from './pages';
+import { DashboardPage, ExploreTripsPage, LoginPage, NotFoundPage, ProfilePage, SignupPage, TripsPage, UserPage } from './pages';
 
 import './App.css'
 
@@ -25,6 +25,9 @@ function App() {
 
         <Route path='/user/:userId'
           element={ <UserPage /> } />
+
+        <Route path='/trips'
+          element={ <ExploreTripsPage /> } />
         
         <Route path='/trips/:tripId' 
           element={ <TripsPage /> } />
