35fef5549db6771cb2b978c0173652b9b3a55e7a
Rafacruiz <rafacruiz@gmail.com>
2026-03-20 22:46:53 +0100

    refactor: useTrips filters and pagination

--- web/src/hooks/use-trips.js
+++ web/src/hooks/use-trips.js
@@ -7,27 +7,31 @@ function useTrips(filters) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [trips, setTrips] = useState(null);
+    const [pagination, setPagination] = useState(null);
 
     useEffect(() => {
-        const timerOut = 400;
-
-        const timer = setTimeout( async() => {
+        const timer = (!filters.search) ? 0 : 500;
+        
+        const timerOut = setTimeout( async() => {
             try {
                 setLoading(true);
+                setTrips(null);
+
                 const trips = await listTrips(filters);
                 setTrips(trips.data);
+                setPagination(trips.pagination);
             } catch (error) {
                 console.log(error);
                 setError(error);
             } finally {
                 setLoading(false);
             }
-        }, timerOut);
+        }, timer);
 
-        return () => clearTimeout(timer);
+        return () => clearTimeout(timerOut);
     }, [filters]);
 
-    return { trips, loading, error };
+    return { trips, loading, error, pagination };
 }
 
 export default useTrips;
