ead873df799d80f9a11865791834b57075fcf226
Rafacruiz <rafacruiz@gmail.com>
2026-03-23 01:02:46 +0100

    feat: new services findByCode for Country

---
+++ api/services/country.service.js
@@ -0,0 +1,19 @@
+
+const getCountryByCode = async (code) => {
+
+    const response = await fetch(
+        new URL(process.env.COUNTRY_API_URL + '/alpha/' + code)
+    );
+
+    if (!response.ok) return { error: 500, message: "Failed to fetch countries" };
+
+    const [country] = await response.json();
+    
+    return { 
+        name: country.name.common, 
+        code: country.cca2, 
+        flag: country.flag || null 
+    };
+}
+
+export default getCountryByCode;
