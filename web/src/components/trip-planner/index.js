b1217302365b6695b279b412e3d0169f61e16f15
rafacruiz <rafacruiz@gmail.com>
2026-03-24 16:02:33 +0100

    delete file

--- web/src/components/trip-planner/index.js
+++
@@ -1,7 +0,0 @@
-
-export { default as TripsList } from './trips/trips-list/trips-list';
-export { default as TripsItem } from './trips/trips-item/trips-item';
-export { default as TripsDetails } from './trips/trips-details/trips-details';
-export { default as TripDate } from './trips/trips-utils/date-trips';
-export { default as TripsSlider } from './trips/trips-slider/trips-slider';
-export { default as TripsSearch } from './trips/trips-search/trips-search';
