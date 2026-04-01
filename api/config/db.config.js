e2b3539ad7694e2ecd67ae8d131d5b499bf8dcab
rafacruiz <rafacruiz@gmail.com>
2026-04-01 15:52:06 +0200

    chore: url db test and production

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,11 +1,12 @@
 
 import mongoose from "mongoose";
 
-let MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/tripplanner-db";
+let MONGODB_URI;
 
 if (process.env.NODE_ENV === "test") {
-  MONGODB_URI += "_test";
+  MONGODB_URI = "mongodb://127.0.0.1:27017/tripplanner-db_test";
+} else {
+  MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/tripplanner-db";
 }
 
 mongoose
